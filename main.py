b6e0b231367265d2cd8027e2ce5d879479e6549b
Michał Hewelt <mhewelt9@gmail.com>
2022-12-10 13:32:34 +0100

    extract colors from a picture

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import colorgram
+
+
+colors = colorgram.extract('hirst.jpg', 10)
+rgb_list = []
+
+index = 0
+for color in colors:
+    rgb = color.rgb
+    red = rgb[0]
+    green = rgb[1]
+    blue = rgb[2]
+    color_tuple = (red, green, blue)
+    rgb_list.append(color_tuple)
+
+print(rgb_list)
 
